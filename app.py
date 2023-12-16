79c6d9e80d8536c8061f6dd532eae2b16b669e23
anibalhsm <98560297+anibalhsm@users.noreply.github.com>
2023-12-16 18:27:31 +0000

    modified:   app.py
    modified:   templates/login.html

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+# Assuming you have a dictionary to store user data
+users = {}
 
 @app.route('/')
 def home():
     return render_template('main.html')
 
-@app.route('/Login')
+@app.route('/Login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Check if user exists and the password is correct
+        if username in users and check_password_hash(users[username], password):
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid username or password'
+    else:
+        return render_template('login.html')
 
 @app.route('/Products')
 def products():
@@ -21,12 +35,6 @@ def contact():
 @app.route('/Shop')
 def shop():
     return render_template('shop.html')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:your_password@your_host/your_database'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
