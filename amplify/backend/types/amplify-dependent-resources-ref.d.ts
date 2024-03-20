ea755cd05ceb2334730cb79aad48454b12695830
anibalhsm <98560297+anibalhsm@users.noreply.github.com>
2024-03-20 14:56:14 +0000

    new file:   .gitignore 	new file:   .graphqlconfig.yml 	modified:   .vscode/settings.json 	new file:   amplify/.config/project-config.json 	new file:   amplify/backend/api/StickyLink/cli-inputs.json new file:   amplify/backend/api/StickyLink/parameters.json 	new file:   amplify/backend/api/StickyLink/resolvers/README.md 	new file:   amplify/backend/api/StickyLink/schema.graphql 	new file:   amplify/backend/api/StickyLink/stacks/CustomResources.json new file:   amplify/backend/api/StickyLink/transform.conf.json 	new file:   amplify/backend/backend-config.json 	new file:   amplify/backend/tags.json 	new file:   amplify/backend/types/amplify-dependent-resources-ref.d.ts new file:   amplify/cli.json 	new file:   amplify/team-provider-info.json 	new file:   install.cmd 	new file:   src/graphql/mutations.js 	new file:   src/graphql/queries.js 	new file:   src/graphql/subscriptions.js 	new file:   src/ui-components/TodoCreateForm.d.ts 	new file:   src/ui-components/TodoCreateForm.jsx 	new file:   src/ui-components/TodoUpdateForm.d.ts 	new file:   src/ui-components/TodoUpdateForm.jsx 	new file:   src/ui-components/index.js 	new file:   src/ui-components/studioTheme.js 	new file:   src/ui-components/studioTheme.js.d.ts 	new file:   src/ui-components/utils.js

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "StickyLink": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
