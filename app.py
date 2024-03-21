9b27b12dea32fcf4ea6bf2817d7dc7b2b23d3daa
anibalhsm <98560297+anibalhsm@users.noreply.github.com>
2024-03-21 15:41:39 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -252,7 +252,8 @@ def contact():
         return redirect(url_for('contact')) 
     else:
         return render_template('contact.html')
-
+    
+app.cli.add_command(promote_to_admin)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
